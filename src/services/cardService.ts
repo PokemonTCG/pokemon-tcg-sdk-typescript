f30b2313fe85b0e4bf6ed8f1310b158ffc79a589
Tee <teedia31@gmail.com>
2021-02-26 08:57:15 -0500

    Remove QueryBuilder abstraction from CardService

---
+++ src/services/cardService.ts
@@ -0,0 +1,26 @@
+import { Query } from "../interfaces/query";
+import { APIService } from "../interfaces/apiService";
+import { Card } from "../interfaces/card";
+import {Client} from "../client";
+
+export class CardService implements APIService {
+    async find(id: string): Promise<Card[]> {
+        const response: Card[] = await Client.get('cards', id);
+        return response;
+    }
+
+    async all(): Promise<Card[]> {
+        const params: Query[] = [{
+            name: 'pageSize',
+            value: 250,
+        }];
+
+        const response: Card[] = await Client.get('cards', params);
+        return response;
+    }
+
+    async where(params: Query[]): Promise<Card[]> {
+        const response: Card[] = await Client.get('cards', params);
+        return response;
+    }
+}

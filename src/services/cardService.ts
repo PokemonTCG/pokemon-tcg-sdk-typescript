a7afa92184241da5b3f3435251cde8460bc94fbc
Tee <teedia31@gmail.com>
2021-02-26 10:58:05 -0500

    Use generic APIService implemention for CardService

--- src/services/cardService.ts
+++ src/services/cardService.ts
@@ -1,13 +1,13 @@
 import { Query } from "../interfaces/query";
 import { APIService } from "../interfaces/apiService";
 import { Card } from "../interfaces/card";
-import {Client} from "../client";
+import { Client } from "../client";
 
-export class CardService implements APIService {
+export class CardService implements APIService<Card> {
     async find(id: string): Promise<Card[]> {
         const response: Card[] = await Client.get('cards', id);
         return response;
-    }
+    };
 
     async all(): Promise<Card[]> {
         const params: Query[] = [{
@@ -17,10 +17,10 @@ export class CardService implements APIService {
 
         const response: Card[] = await Client.get('cards', params);
         return response;
-    }
+    };
 
-    async where(params: Query[]): Promise<Card[]> {
+   async where(params: Query[]): Promise<Card[]> {
         const response: Card[] = await Client.get('cards', params);
         return response;
-    }
+    };
 }

e7d36cc5a1f6e908ae08ba66a85d5e69a405b475
Tee <teedia31@gmail.com>
2021-02-28 23:45:36 -0500

    Use default exports for Set service methods instead of classes

--- src/services/setService.ts
+++ src/services/setService.ts
@@ -1,16 +1,15 @@
 import { Query } from "../interfaces/query";
-import { APIService } from "../interfaces/apiService";
 import { Set } from "../interfaces/set";
 import { Client } from "../client";
 
-export class SetService implements APIService<Set> {
-    async find(id: string): Promise<Set[]> {
+export default {
+    findSetByID: async function (id: string): Promise<Set> {
         const client: Client = Client.getInstance();
-        const response: Set[] = await client.get<Set[]>('sets', id);
+        const response: Set = await client.get<Set>('sets', id);
         return response;
-    };
+    },
 
-    async all(): Promise<Set[]> {
+    getAllSets: async function (): Promise<Set[]> {
         const params: Query[] = [{
             name: 'pageSize',
             value: 250,
@@ -19,11 +18,11 @@ export class SetService implements APIService<Set> {
         const client: Client = Client.getInstance();
         const response: Set[] = await client.get<Set[]>('sets', params);
         return response;
-    };
+    },
 
-    async where(params: Query[]): Promise<Set[]> {
+    findSetByQueries: async function(params: Query[]): Promise<Set[]> {
         const client: Client = Client.getInstance();
         const response: Set[] = await client.get<Set[]>('sets', params);
         return response;
-    };
+    }
 }

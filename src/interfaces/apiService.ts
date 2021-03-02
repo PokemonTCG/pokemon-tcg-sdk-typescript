b12e306c7324d8a624cf8316ed99b2486898145f
Tee <teedia31@gmail.com>
2021-03-01 22:00:14 -0500

    Remove API service interface

--- src/interfaces/apiService.ts
+++
@@ -1,7 +0,0 @@
-import { Query } from "./query";
-
-export interface APIService<T> {
-    find: (id: string) => Promise<T[]>;
-    all: () => Promise<T[]>;
-    where: (params: Query[]) => Promise<T[]>;
-}

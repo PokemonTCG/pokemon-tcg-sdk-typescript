11c0c9041f41c506fe486e2304cb0d0cb6ffacc8
Tee <teedia31@gmail.com>
2021-03-12 21:58:57 -0500

    Add type enums

---
+++ src/enums/types.ts
@@ -0,0 +1,13 @@
+export enum Types {
+    Colorless = "Colorless",
+    Darkness = "Darkness",
+    Dragon = "Dragon",
+    Fairy = "Fairy",
+    Fighting = "Fighting",
+    Fire = "Fire",
+    Grass = "Grass",
+    Lightening = "Lightening",
+    Metal = "Metal",
+    Psychic = "Psychic",
+    Water = "Water"
+}

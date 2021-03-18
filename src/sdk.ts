8293febb190c34b9fe4a7546f018251231b2d5bc
Tee <teedia31@gmail.com>
2021-03-18 09:24:12 -0400

    Export parameter interface

--- src/sdk.ts
+++ src/sdk.ts
@@ -2,7 +2,7 @@
 export * from './interfaces/ability';
 export * from './interfaces/attack';
 export * from './interfaces/card';
-export * from './interfaces/query';
+export * from './interfaces/parameter';
 export * from './interfaces/stats';
 
 // Enums
@@ -10,7 +10,6 @@ export * from './enums/type';
 export * from './enums/supertype';
 export * from './enums/subtype';
 export * from './enums/rarity';
-export * from './enums/parameter';
 
 // Services
 export * from './services/cardService';

21d94dd99743ea9875ab70c3bd1c80e0bf03dbfb
Tee <teedia31@gmail.com>
2021-03-22 22:26:08 -0400

    Refactor meta route retrieval and return types

--- src/classes/meta.ts
+++ src/classes/meta.ts
@@ -2,18 +2,22 @@ import { Client } from '../client';
 
 export class Meta {
   static async allTypes(): Promise<string[]> {
-    return Client.get('types');
+    const resposne = await Client.get('types');
+    return resposne;
   }
 
   static async allSubtypes(): Promise<string[]> {
-    return Client.get('subtypes');
+    const response = await Client.get('subtypes');
+    return response;
   }
 
   static async allSupertypes(): Promise<string[]> {
-    return Client.get('supertypes');
+    const response = await Client.get('supertypes');
+    return response;
   }
 
-  static allRarities(): Promise<string[]> {
-    return Client.get('rarities');
+  static async allRarities(): Promise<string[]> {
+    const response = await Client.get('rarities');
+    return response;
   }
 }

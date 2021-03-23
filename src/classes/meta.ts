f077348af4a09fc44bae567d1e81cb89ac8d1488
Tee <teedia31@gmail.com>
2021-03-22 22:31:06 -0400

    Add deprication notices to Meta methods

--- src/classes/meta.ts
+++ src/classes/meta.ts
@@ -1,21 +1,33 @@
 import { Client } from '../client';
 
 export class Meta {
+  /**
+   * @deprecated `allTypes` is renamed to `getAllTypes` in version 2. See the **[migration guide](https://github.com/acupoftee/pokemon-tcg-sdk-typescript/blob/master/MIGRATING.md)** for more info.
+   */
   static async allTypes(): Promise<string[]> {
     const resposne = await Client.get('types');
     return resposne;
   }
 
+  /**
+   * @deprecated `allSubtypes` is renamed to `getAllSubtypes` in version 2. See the **[migration guide](https://github.com/acupoftee/pokemon-tcg-sdk-typescript/blob/master/MIGRATING.md)** for more info.
+   */
   static async allSubtypes(): Promise<string[]> {
     const response = await Client.get('subtypes');
     return response;
   }
 
+  /**
+   * @deprecated `allSupertypes` is renamed to `getAllSupertypes` in version 2. See the **[migration guide](https://github.com/acupoftee/pokemon-tcg-sdk-typescript/blob/master/MIGRATING.md)** for more info.
+   */
   static async allSupertypes(): Promise<string[]> {
     const response = await Client.get('supertypes');
     return response;
   }
 
+  /**
+   * @deprecated `allRarities` is renamed to `getAllRarities` in version 2. See the **[migration guide](https://github.com/acupoftee/pokemon-tcg-sdk-typescript/blob/master/MIGRATING.md)** for more info.
+   */
   static async allRarities(): Promise<string[]> {
     const response = await Client.get('rarities');
     return response;

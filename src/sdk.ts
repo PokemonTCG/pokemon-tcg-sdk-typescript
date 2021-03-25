520a2116c3df12f647051a9874dc0185f9a79bb3
Tee <teedia31@gmail.com>
2021-03-24 20:08:00 -0400

    Export remaining v2 interfaces

--- src/sdk.ts
+++ src/sdk.ts
@@ -4,6 +4,10 @@ export * from './interfaces/attack';
 export * from './interfaces/card';
 export * from './interfaces/parameter';
 export * from './interfaces/stats';
+export * from './interfaces/set';
+export * from './interfaces/image';
+export * from './interfaces/tcgplayer';
+export * from './interfaces/legality';
 
 // Enums
 export * from './enums/type';
